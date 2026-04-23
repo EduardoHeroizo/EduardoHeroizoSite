638226a847a44eef73929632c871e3d0d704c92c
EduardoHeroizo <eduardoheroizo1@hotmail.com>
2026-04-23 19:16:59 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AboutMe from "./components/AboutMe"
+
+function App() {
+    
+    return (
+        
+        <div className="bg-amber-100 w-screen h-screen flex justify-start p-6"> 
+            <AboutMe />
+        </div>
+    
+    )
+}
+
+export default App
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,21 @@
+function AboutMe() {
+  return (
+    <div className="bg-white border border-black rounded-xl shadow-2xl w-[400px] min-h-[600px] p-6 flex flex-col items-center">
+      
+      <h1 className="font-comic text-3xl font-bold mb-4">
+        About Me
+      </h1>
+
+      <p className="font-comic text-lg leading-relaxed text-center text-black/70">
+        Hello, my name is Eduardo. I am a Computer Science undergraduate at the Federal University of Campina Grande (UFCG),
+        currently in my fourth semester, with a strong focus on software development and data-driven problem solving.
+        I have been building a solid foundation in algorithms, data structures, and software engineering principles,
+        and I am continuously seeking opportunities to apply my knowledge in practical projects and refine my technical skills,
+        aiming to develop efficient and scalable systems as a future Software Engineer.
+      </p>
+
+    </div>
+  )
+}
+
+export default AboutMe
