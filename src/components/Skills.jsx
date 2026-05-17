71051f9a451f182578c7e34a7d2f7cd00dbe4c0d
EduardoHeroizo <eduardoheroizo1@hotmail.com>
2026-05-17 19:31:41 -0300

    feat: add icons skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,24 +1,30 @@
 import javaIcon from "../assets/skills/java.svg";
-import springBootIcon from "../assets/skills/spring_boot.svg.png";
+import springBootIcon from "../assets/skills/Spring_Boot.svg.png";
 import javaScriptIcon from "../assets/skills/javascript.svg";
-import reactIcon from "../assets/skills/react.svg";
-import git from "../assets/skills/git.svg";
-import python from "../assets/skills/python.png";
+import reactIcon from "../assets/skills/React.svg";
+import gitIcon from "../assets/skills/git.svg";
+import pythonIcon from "../assets/skills/python.svg";
+import sqlIcon from "../assets/skills/sql.png";
+import scikitLearnIcon from "../assets/skills/scikit-learn.svg";
+import dockerIcon from "../assets/skills/docker.svg";
+import pandasIcon from "../assets/skills/pandas.svg";
+import postgresIcon from "../assets/skills/posgresql.png";
+import linuxIcon from "../assets/skills/linux.png";
 
 function Skills() {
   return (
-    <div className="bg-white border border-black rounded-xl shadow-2xl w-[500px] min-h-[250px] p-6 flex flex-col items-center">
-      <h1 className="font-comic text-3xl font-bold mb-4">
+    <div className="bg-white border border-black rounded-xl shadow-2xl w-[560px] min-h-[250px] p-6 flex flex-col items-center">
+      <h1 className="font-comic text-3xl font-bold mb-6">
         Skills
       </h1>
 
-      <div className="flex flex-row items-center gap-2">
+      <div className="grid grid-cols-6 gap-x-4 gap-y-5 items-center justify-items-center w-full">
         {/* Java */}
-        <div className="relative group">
+        <div className="relative group w-16 h-16 flex items-center justify-center">
           <img
             src={javaIcon}
             alt="Java"
-            className="w-20 h-20 transition-transform duration-300 hover:scale-110"
+            className="w-[76px] h-[76px] transition-transform duration-300 hover:scale-110"
           />
 
           <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
@@ -31,13 +37,13 @@ function Skills() {
             </div>
 
             <p className="font-comic text-xl text-black/70 mt-2 text-center">
-              Good knowledge
+              Good Knowledge
             </p>
           </div>
         </div>
 
         {/* Spring Boot */}
-        <div className="relative group">
+        <div className="relative group w-16 h-16 flex items-center justify-center">
           <img
             src={springBootIcon}
             alt="Spring Boot"
@@ -60,11 +66,11 @@ function Skills() {
         </div>
 
         {/* JavaScript */}
-        <div className="relative group">
+        <div className="relative group w-16 h-16 flex items-center justify-center">
           <img
             src={javaScriptIcon}
             alt="JavaScript"
-            className="w-16 h-16 ml-4 transition-transform duration-300 hover:scale-110"
+            className="w-16 h-16 transition-transform duration-300 hover:scale-110"
           />
 
           <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
@@ -83,11 +89,11 @@ function Skills() {
         </div>
 
         {/* React */}
-        <div className="relative group">
+        <div className="relative group w-16 h-16 flex items-center justify-center">
           <img
             src={reactIcon}
             alt="React"
-            className="w-16 h-16 ml-4 transition-transform duration-300 hover:scale-110"
+            className="w-16 h-16 transition-transform duration-300 hover:scale-110"
           />
 
           <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
@@ -104,13 +110,13 @@ function Skills() {
             </p>
           </div>
         </div>
-        
+
         {/* Git */}
-         <div className="relative group">
+        <div className="relative group w-16 h-16 flex items-center justify-center">
           <img
-            src={git}
+            src={gitIcon}
             alt="Git"
-            className="w-16 h-16 ml-4 transition-transform duration-300 hover:scale-110"
+            className="w-16 h-16 transition-transform duration-300 hover:scale-110"
           />
 
           <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
@@ -129,11 +135,11 @@ function Skills() {
         </div>
 
         {/* Python */}
-        <div className="relative group">
+        <div className="relative group w-16 h-16 flex items-center justify-center">
           <img
-            src={python}
+            src={pythonIcon}
             alt="Python"
-            className="w-14 h-18 ml-6 transition-transform duration-300 hover:scale-110"
+            className="w-16 h-16 transition-transform duration-300 hover:scale-110"
           />
 
           <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
@@ -151,7 +157,148 @@ function Skills() {
           </div>
         </div>
 
+        {/* SQL */}
+        <div className="relative group w-16 h-16 flex items-center justify-center">
+          <img
+            src={sqlIcon}
+            alt="SQL"
+            className="w-14 h-14 transition-transform ml-3 duration-300 hover:scale-110"
+          />
+
+          <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
+            <p className="font-comic text-xl text-black mb-2 text-center">
+              SQL
+            </p>
+
+            <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+              <div className="h-full w-[75%] bg-green-500 rounded-full"></div>
+            </div>
+
+            <p className="font-comic text-xl text-black/70 mt-2 text-center">
+              Good Knowledge
+            </p>
+          </div>
+        </div>
+
+        {/* Scikit-learn */}
+        <div className="relative group w-16 h-16 flex items-center justify-center">
+          <img
+            src={scikitLearnIcon}
+            alt="Scikit-learn"
+            className="w-16 h-16 transition-transform duration-300 hover:scale-110"
+          />
+
+          <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
+            <p className="font-comic text-xl text-black mb-2 text-center">
+              Scikit-learn
+            </p>
+
+            <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+              <div className="h-full w-[46%] bg-green-500 rounded-full"></div>
+            </div>
+
+            <p className="font-comic text-xl text-black/70 mt-2 text-center">
+              Intermediate
+            </p>
+          </div>
+        </div>
+
+        {/* Docker */}
+        <div className="relative group w-16 h-16 flex items-center justify-center">
+          <img
+            src={dockerIcon}
+            alt="Docker"
+            className="w-16 h-16 transition-transform duration-300 hover:scale-110"
+          />
+
+          <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
+            <p className="font-comic text-xl text-black mb-2 text-center">
+              Docker
+            </p>
+
+            <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+              <div className="h-full w-[20%] bg-green-500 rounded-full"></div>
+            </div>
+
+            <p className="font-comic text-xl text-black/70 mt-2 text-center">
+              Beginner
+            </p>
+          </div>
+        </div>
+
+        {/* Pandas */}
+        <div className="relative group w-16 h-16 flex items-center justify-center">
+          <img
+            src={pandasIcon}
+            alt="Pandas"
+            className="w-16 h-16 transition-transform duration-300 hover:scale-110"
+          />
+
+          <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
+            <p className="font-comic text-xl text-black mb-2 text-center">
+              Pandas
+            </p>
+
+            <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+              <div className="h-full w-[47%] bg-green-500 rounded-full"></div>
+            </div>
+
+            <p className="font-comic text-xl text-black/70 mt-2 text-center">
+              Intermediate
+            </p>
+          </div>
+        </div>
+
+        {/* PostgreSQL */}
+        <div className="relative group w-16 h-16 flex items-center justify-center">
+          <img
+            src={postgresIcon}
+            alt="PostgreSQL"
+            className="w-16 h-16 transition-transform duration-300 hover:scale-110"
+          />
+
+          <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
+            <p className="font-comic text-xl text-black mb-2 text-center">
+              PostgreSQL
+            </p>
+
+            <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+              <div className="h-full w-[21%] bg-green-500 rounded-full"></div>
+            </div>
+
+            <p className="font-comic text-xl text-black/70 mt-2 text-center">
+              Beginner
+            </p>
+          </div>
+        </div>
+
+        {/* Linux */}
+        <div className="relative group w-22 h-16 flex items-center justify-center">
+          <img
+            src={linuxIcon}
+            alt="Linux"
+            className="w-22 h-20 transition-transform duration-300 hover:scale-110"
+          />
+
+          <div className="absolute left-1/2 -translate-x-1/2 top-20 w-40 bg-white border border-black rounded-lg shadow-lg p-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
+            <p className="font-comic text-xl text-black mb-2 text-center">
+              Linux
+            </p>
+
+            <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+              <div className="h-full w-[42%] bg-green-500 rounded-full"></div>
+            </div>
+
+            <p className="font-comic text-xl text-black/70 mt-2 text-center">
+              Intermediate
+            </p>
+          </div>
+        </div>
       </div>
+
+    <div className="w-full h-[1px] bg-gray-300 mt-3" />
+
+
     </div>
   );
 }
