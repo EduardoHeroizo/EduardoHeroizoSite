5781ca5f6caddce7af629b54fe2aa5952630e26b
EduardoHeroizo <eduardoheroizo1@hotmail.com>
2026-05-17 22:49:25 -0300

    Improve mobile responsivess App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,22 @@ import Experience from "./components/Experience";
 
 function App() {
   return (
-    <div className="bg-amber-100 bg-[radial-gradient(#00000035_1.5px,transparent_1.5px)] [background-size:24px_24px] min-h-screen p-6">
-      <div className="max-w-[1760px] mx-auto grid grid-cols-[500px_560px_560px] gap-x-14 items-start">
+    <div className="bg-amber-100 bg-[radial-gradient(#00000035_1.5px,transparent_1.5px)] [background-size:24px_24px] min-h-screen px-4 py-6 overflow-x-hidden">
+      <div className="w-full max-w-[1760px] mx-auto grid grid-cols-1 xl:grid-cols-[500px_560px_560px] gap-6 xl:gap-x-14 items-start justify-items-center">
         
-        <div className="flex flex-col gap-y-4">
+        <div className="w-full max-w-[500px] flex flex-col gap-y-4 items-center">
           <AboutMe />
           <ContactMe />
         </div>
 
-        <div className="flex flex-col gap-y-4">
+        <div className="w-full max-w-[560px] flex flex-col gap-y-4 items-center">
           <Skills />
           <SoftSkills />
         </div>
 
-        <div className="flex flex-col gap-y-4">
+        <div className="w-full max-w-[560px] flex flex-col gap-y-4 items-center">
           <Download />
-          <Experience/>
+          <Experience />
         </div>
 
       </div>
